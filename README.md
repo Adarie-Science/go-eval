c5ce0e4bd04c4ba14b92b5862b3b4ad3341c567f
Peter de Blanc <peter@spaceandgames.com>
2025-04-21 11:20:28 +0900

    Add quiz script.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Datasets
 
 `examples/set0.jsonl`: This is a small dataset to be used for integration testing (i.e. making sure the eval setup is working) -- not to be used as a model eval.
+
+# Script usage examples
+
+- `python generate_data.py 30 examples/myset.jsonl` -- Generate 30 test cases and save to `examples/myset.jsonl`
+
+- `python quiz.py examples/myset.jsonl 10` -- Quiz the user (i.e. presumably a human) on 10 test cases previously saved to `examples/myset.jsonl`
---
+++ quiz.py
@@ -0,0 +1,68 @@
+import json
+import random
+import sys
+
+
+def parse_args() -> tuple[str, int]:
+    if len(sys.argv) < 2:
+        raise ValueError
+    examples_filename = sys.argv[1]
+    assert examples_filename.endswith(".jsonl")
+    count = int(sys.argv[2]) if len(sys.argv) > 2 else 10
+    return examples_filename, count
+
+
+def score_p(p: float) -> float:
+    return 1 - (1 - p) ** 2
+
+
+def attempt_problem(problem) -> tuple[float, float]:
+    prompt, policy = problem["prompt"], problem["policy"]
+    print(prompt)
+    guess = input().strip().upper()
+    if guess not in policy:
+        print("Invalid move: -1 point.")
+        return -1
+    score = score_p(policy[guess])
+    best_move, best_p = max(policy.items(), key=lambda item: item[1])
+    best_score = score_p(best_p)
+    if best_score == score:
+        print(f"Perfect! {score:.3f} points.")
+    else:
+        print(
+            f"Best move was {best_move}. Score: {score:.3f} out of possible {best_score:.3f}"
+        )
+    return best_score, best_score
+
+
+def main():
+    try:
+        examples_filename, count = parse_args()
+    except (AssertionError, ValueError):
+        print(
+            "Usage: python quiz.py <examples_filename.jsonl> [number of examples to try]"
+        )
+    with open(examples_filename, "r") as f:
+        examples = [json.loads(line) for line in f]
+        print(f"Loaded {len(examples)} examples from {examples_filename}...")
+        random.shuffle(examples)
+        if len(examples) < count:
+            print(
+                f"{count} examples were requested, but only {len(examples)} are available."
+            )
+            count = len(examples)
+        print(f"Beginning quiz on {count} problems...")
+        score = 0
+        max_score = 0
+        for example in examples[:count]:
+            p_score, max_p_score = attempt_problem(example)
+            score += p_score
+            max_score += max_p_score
+        print()
+        print(
+            f"Problem set complete! Total score: {score:.3f} out of a possible {max_score:.3f} points."
+        )
+
+
+if __name__ == "__main__":
+    main()
