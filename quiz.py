52c223914af43565482f9b4bba83615aa46c5fd6
Peter de Blanc <peter@spaceandgames.com>
2025-04-25 09:20:09 +0900

    Allow user to specify prompt style in quiz.py.

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import random
 import sys
@@ -16,9 +17,9 @@ def score_p(p: float) -> float:
     return 1 - (1 - p) ** 2
 
 
-def attempt_problem(problem) -> tuple[float, float]:
+def attempt_problem(problem, prompt_key) -> tuple[float, float]:
     print()
-    prompt, policy = problem["prompts"]["ansi"], problem["policy"]
+    prompt, policy = problem["prompts"][prompt_key], problem["policy"]
     guess = input(prompt + " ").strip().upper()
     if guess not in policy:
         print("Invalid move: -1 point.")
@@ -35,27 +36,32 @@ def attempt_problem(problem) -> tuple[float, float]:
     return score, best_score
 
 
+parser = argparse.ArgumentParser(prog="Adarie Go Eval Quiz")
+parser.add_argument("filename")
+parser.add_argument("-c", "--count", type=int)
+parser.add_argument("-p", "--prompt", choices=["ascii", "ansi", "gtp"], default="ansi")
+
+
 def main():
-    try:
-        examples_filename, count = parse_args()
-    except (AssertionError, ValueError):
-        print(
-            "Usage: python quiz.py <examples_filename.jsonl> [number of examples to try]"
-        )
-    with open(examples_filename, "r") as f:
+    args = parser.parse_args()
+
+    with open(args.filename, "r") as f:
         examples = [json.loads(line) for line in f]
-        print(f"Loaded {len(examples)} examples from {examples_filename}...")
+        print(f"Loaded {len(examples)} examples from {args.filename}...")
         random.shuffle(examples)
-        if len(examples) < count:
+        count = args.count
+        if count and len(examples) < count:
             print(
                 f"{count} examples were requested, but only {len(examples)} are available."
             )
+            count = len(examples)
+        if not count:
             count = len(examples)
         print(f"Beginning quiz on {count} problems...")
         score = 0
         max_score = 0
         for example in examples[:count]:
-            p_score, max_p_score = attempt_problem(example)
+            p_score, max_p_score = attempt_problem(example, args.prompt)
             score += p_score
             max_score += max_p_score
         print()
